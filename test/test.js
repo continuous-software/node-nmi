7a402a8f3a9417069461c1937e312ded95887d5f
lorenzofox3 <laurent34azerty@gmail.com>
2014-12-09 14:36:32 +0700

    Add support for authorization

--- test/test.js
+++ test/test.js
@@ -5,179 +5,250 @@ var GatewayError = require('42-cent-base').GatewayError;
 
 describe('NMI adpator', function () {
 
-    var service;
+  var service;
 
-    beforeEach(function () {
-        service = factory(config);
-    });
+  beforeEach(function () {
+    service = factory(config);
+  });
 
+  describe('submitting transaction', function () {
 
-    describe('submitting transaction', function () {
+    it('should submit transaction ', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
 
-        it('should submit transaction ', function (done) {
-            var order = {
-                amount: Math.random() * 100 + 1
-            };
+      var cc = {
+        creditCardNumber: '4111111111111111',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+      var prospect = {
+        customerFirstName: 'Ellen',
+        customerLastName: 'Johson',
+        billingAddress: '14 Main Street',
+        billingCity: 'Pecan Springs',
+        billingZip: '44628',
+        billingState: 'TX',
+        shippingFirstName: 'China',
+        shippingLastName: 'Bayles',
+        shippingAddress: '12 Main Street',
+        shippingCity: 'Pecan Springs',
+        shippingZip: '44628'
+      };
 
-            var cc = {
-                creditCardNumber: '4111111111111111',
-                expirationYear: '17',
-                expirationMonth: '01',
-                cvv: '123'
-            };
-            var prospect = {
-                customerFirstName: 'Ellen',
-                customerLastName: 'Johson',
-                billingAddress: '14 Main Street',
-                billingCity: 'Pecan Springs',
-                billingZip: '44628',
-                billingState: 'TX',
-                shippingFirstName: 'China',
-                shippingLastName: 'Bayles',
-                shippingAddress: '12 Main Street',
-                shippingCity: 'Pecan Springs',
-                shippingZip: '44628'
-            };
-
-            service.submitTransaction(order, cc, prospect)
-                .then(function (result) {
-                    assert(result.transactionId, 'transaction should be defined');
-                    assert(result.authCode, 'authCode should be defined');
-                    assert(result._original, '_original should be defined');
-                    done();
-                });
-        });
-
-        it('should reject the promise if gateway does not return appropriate result', function (done) {
-            var order = {
-                amount: Math.random() * 100 + 1
-            };
-
-            var cc = {
-                creditCardNumber: '666',
-                expirationYear: '17',
-                expirationMonth: '01',
-                cvv: '123'
-            };
-            var prospect = {
-                customerFirstName: 'Ellen',
-                customerLastName: 'Johson',
-                billingAddress: '14 Main Street',
-                billingCity: 'Pecan Springs',
-                billingZip: '44628',
-                billingState: 'TX',
-                shippingFirstName: 'China',
-                shippingLastName: 'Bayles',
-                shippingAddress: '12 Main Street',
-                shippingCity: 'Pecan Springs',
-                shippingZip: '44628'
-            };
-
-            service.submitTransaction(order, cc, prospect)
-                .then(function () {
-                    throw new Error('should not get here')
-                }, function (err) {
-                    assert(err instanceof GatewayError);
-                    assert(err.message.indexOf('Invalid Credit Card Number') !== -1);
-                    assert(err._original, '_original should be defined');
-                    done();
-                });
+      service.submitTransaction(order, cc, prospect)
+        .then(function (result) {
+          assert(result.transactionId, 'transaction should be defined');
+          assert(result.authCode, 'authCode should be defined');
+          assert(result._original, '_original should be defined');
+          done();
         });
     });
 
-    describe('refunding transaction', function () {
+    it('should reject the promise if gateway does not return appropriate result', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
 
-        it('should refund an already settled transaction', function (done) {
-            var order = {
-                amount: Math.random() * 100 + 1
-            };
+      var cc = {
+        creditCardNumber: '666',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+      var prospect = {
+        customerFirstName: 'Ellen',
+        customerLastName: 'Johson',
+        billingAddress: '14 Main Street',
+        billingCity: 'Pecan Springs',
+        billingZip: '44628',
+        billingState: 'TX',
+        shippingFirstName: 'China',
+        shippingLastName: 'Bayles',
+        shippingAddress: '12 Main Street',
+        shippingCity: 'Pecan Springs',
+        shippingZip: '44628'
+      };
 
-            var cc = {
-                creditCardNumber: '4111111111111111',
-                expirationYear: '17',
-                expirationMonth: '01',
-                cvv: '123'
-            };
-            var prospect = {
-                customerFirstName: 'Ellen',
-                customerLastName: 'Johson',
-                billingAddress: '14 Main Street',
-                billingCity: 'Pecan Springs',
-                billingZip: '44628',
-                billingState: 'TX',
-                shippingFirstName: 'China',
-                shippingLastName: 'Bayles',
-                shippingAddress: '12 Main Street',
-                shippingCity: 'Pecan Springs',
-                shippingZip: '44628'
-            };
-
-            var transId;
-
-            service.submitTransaction(order, cc, prospect)
-                .then(function (result) {
-
-                    var transId = result.transactionId;
-
-                    return service.refundTransaction(transId);
-                })
-                .then(function (result) {
-                    assert(result._original, 'original should be defined');
-                    done();
-                });
+      service.submitTransaction(order, cc, prospect)
+        .then(function () {
+          throw new Error('should not get here')
+        }, function (err) {
+          assert(err instanceof GatewayError);
+          assert(err.message.indexOf('Invalid Credit Card Number') !== -1);
+          assert(err._original, '_original should be defined');
+          done();
         });
+    });
+  });
 
-        it('should reject the promise if the gateway does not return appropriate result', function (done) {
-            service.refundTransaction('666')
-                .then(function (result) {
-                    throw new Error('should not get here');
-                }, function (err) {
-                    assert(err instanceof GatewayError);
-                    assert(err.message.indexOf('Transaction not found') !== -1);
-                    assert(err._original, '_original should be defined');
-                    done();
-                })
+  describe('authorize transaction', function () {
+
+    it('should authorize a transaction ', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
+
+      var cc = {
+        creditCardNumber: '4111111111111111',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+      var prospect = {
+        customerFirstName: 'Ellen',
+        customerLastName: 'Johson',
+        billingAddress: '14 Main Street',
+        billingCity: 'Pecan Springs',
+        billingZip: '44628',
+        billingState: 'TX',
+        shippingFirstName: 'China',
+        shippingLastName: 'Bayles',
+        shippingAddress: '12 Main Street',
+        shippingCity: 'Pecan Springs',
+        shippingZip: '44628'
+      };
+
+      service.authorizeTransaction(order, cc, prospect)
+        .then(function (result) {
+          assert(result.transactionId, 'transaction should be defined');
+          assert(result.authCode, 'authCode should be defined');
+          assert(result._original, '_original should be defined');
+          done();
+        });
+    });
+
+    it('should reject the promise if gateway does not return appropriate result', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
+
+      var cc = {
+        creditCardNumber: '666',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+      var prospect = {
+        customerFirstName: 'Ellen',
+        customerLastName: 'Johson',
+        billingAddress: '14 Main Street',
+        billingCity: 'Pecan Springs',
+        billingZip: '44628',
+        billingState: 'TX',
+        shippingFirstName: 'China',
+        shippingLastName: 'Bayles',
+        shippingAddress: '12 Main Street',
+        shippingCity: 'Pecan Springs',
+        shippingZip: '44628'
+      };
+
+      service.authorizeTransaction(order, cc, prospect)
+        .then(function () {
+          throw new Error('should not get here')
+        }, function (err) {
+          assert(err instanceof GatewayError);
+          assert(err.message.indexOf('Invalid Credit Card Number') !== -1);
+          assert(err._original, '_original should be defined');
+          done();
+        });
+    });
+  });
+
+  describe('refunding transaction', function () {
+
+    it('should refund an already settled transaction', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
+
+      var cc = {
+        creditCardNumber: '4111111111111111',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+      var prospect = {
+        customerFirstName: 'Ellen',
+        customerLastName: 'Johson',
+        billingAddress: '14 Main Street',
+        billingCity: 'Pecan Springs',
+        billingZip: '44628',
+        billingState: 'TX',
+        shippingFirstName: 'China',
+        shippingLastName: 'Bayles',
+        shippingAddress: '12 Main Street',
+        shippingCity: 'Pecan Springs',
+        shippingZip: '44628'
+      };
+
+      var transId;
+
+      service.submitTransaction(order, cc, prospect)
+        .then(function (result) {
+
+          var transId = result.transactionId;
+
+          return service.refundTransaction(transId);
         })
-
-    });
-
-    describe('void transaction', function () {
-        it('should void a transaction', function (done) {
-            var order = {
-                amount: Math.random() * 100 + 1
-            };
-
-            var cc = {
-                creditCardNumber: '4111111111111111',
-                expirationYear: '17',
-                expirationMonth: '01',
-                cvv: '123'
-            };
-
-            var transId;
-            service.submitTransaction(order, cc)
-                .then(function (result) {
-                    transId = result.transactionId;
-                    return service.voidTransaction(transId);
-                })
-                .then(function (response) {
-                    assert(response._original, '_original should be defined');
-                    done();
-                });
-        });
-
-        it('should reject the promise when the gateway returns error', function (done) {
-            service.voidTransaction(666)
-                .then(function (res) {
-                    throw new Error('should not get here');
-                }, function (err) {
-                    assert(err instanceof GatewayError);
-                    assert(err.message.indexOf('Transaction not found') !== -1);
-                    assert(err._original, '_original should be defined');
-                    done();
-                })
+        .then(function (result) {
+          assert(result._original, 'original should be defined');
+          done();
         });
     });
 
+    it('should reject the promise if the gateway does not return appropriate result', function (done) {
+      service.refundTransaction('666')
+        .then(function (result) {
+          throw new Error('should not get here');
+        }, function (err) {
+          assert(err instanceof GatewayError);
+          assert(err.message.indexOf('Transaction not found') !== -1);
+          assert(err._original, '_original should be defined');
+          done();
+        })
+    })
+
+  });
+
+  describe('void transaction', function () {
+    it('should void a transaction', function (done) {
+      var order = {
+        amount: Math.random() * 100 + 1
+      };
+
+      var cc = {
+        creditCardNumber: '4111111111111111',
+        expirationYear: '17',
+        expirationMonth: '01',
+        cvv: '123'
+      };
+
+      var transId;
+      service.submitTransaction(order, cc)
+        .then(function (result) {
+          transId = result.transactionId;
+          return service.voidTransaction(transId);
+        })
+        .then(function (response) {
+          assert(response._original, '_original should be defined');
+          done();
+        });
+    });
+
+    it('should reject the promise when the gateway returns error', function (done) {
+      service.voidTransaction(666)
+        .then(function (res) {
+          throw new Error('should not get here');
+        }, function (err) {
+          assert(err instanceof GatewayError);
+          assert(err.message.indexOf('Transaction not found') !== -1);
+          assert(err._original, '_original should be defined');
+          done();
+        })
+    });
+  });
 
 });
